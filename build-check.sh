f7acb556869ec1adccbcce05fc751074292e3098
helloworldlv <775331175@qq.com>
2023-09-01 17:23:28 +0800

    build-check

--- build-check.sh
+++ build-check.sh
@@ -3,14 +3,14 @@
 cd ./physx
 ./generate_projects.sh emscripten
 
-cd ./compiler/emscripten-debug
+cd ./compiler/emscripten-check
 emmake make -j4 #thread number
 
 cd ../../../
 mkdir wasm_build
 cd wasm_build
 ln -sf ../physx/source/physxwebbindings/src/PxWebBindings.cpp .
-cp ../physx/bin/UNKNOWN/debug/physx.debug.js . 
+cp ../physx/bin/UNKNOWN/check/physx.check.js . 
 #cp ../physx/bin/emscripten/release/physx.release.js . 
-cp ../physx/bin/UNKNOWN/debug/physx.debug.wasm .
+cp ../physx/bin/UNKNOWN/check/physx.check.wasm .
 #cp ../physx/bin/emscripten/release/physx.release.wasm .
